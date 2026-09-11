d23c7b415df24ecd51de2ea7d26f0254502f41eb
gwjp19 <gwjporter@icloud.com>
2026-09-11 14:29:35 -0400

    Update Data_Imports.py

--- Data_Imports.py
+++ Data_Imports.py
@@ -4,7 +4,7 @@ import sqlite3
 api_url = "https://ncaa-api.henrygd.me"
 
 game_id = 6305900
-response = request.get(f"{api_url}/game/{game_id}/team-stats"
+response = requests.get(f"{api_url}/game/{game_id}/team-stats"
                       )
 
 response.raise_for_status()
