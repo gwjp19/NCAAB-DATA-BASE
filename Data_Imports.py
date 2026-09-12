c61f78d82fcbff8c64662fa00daf9cb1463a82a0
gwjp19 <gwjporter@icloud.com>
2026-09-12 17:54:18 -0400

    Update Data_Imports.py

--- Data_Imports.py
+++ Data_Imports.py
@@ -20,7 +20,7 @@ for date_info in data["data"]["schedules"]["games"]:
   #print(scoreboard["games"][0])
   for game in scoreboard["games"]:
     game_id = game["game"]["gameID"]
-    game_id_url = f"https://ncaa-api.henrygd.me/game-stats/basketball-men/d1/{game_id}"
+    game_id_url = f"https://ncaa-api.henrygd.me/game/{game_id}/team-stats"
     game_id_response = requests.get(game_id_url)
     game_id_response.raise_for_status()
     game_stats = game_id_response.json()
