b3fec1dc900d02eea34b9b207082cdcdbfa2d525
gwjp19 <gwjporter@icloud.com>
2026-09-12 14:59:34 -0400

    Update Data_Imports.py

--- Data_Imports.py
+++ Data_Imports.py
@@ -17,10 +17,11 @@ for date_info in data["data"]["schedules"]["games"]:
   response = requests.get(url)
   response.raise_for_status()
   scoreboard = response.json()
-  print(scoreboard["games"][0])
+  #print(scoreboard["games"][0])
   for game in scoreboard["games"]:
     game_id = game["game"]["gameID"]
-    print(game_id)
+    print(game["game"]["home"])
+    print(game["game"]["away"])
 
 for team in data["scoreboard"]:
   team_id = team["teamID"]
